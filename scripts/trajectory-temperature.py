aefb34cb62c5d4effcce6029e27216dd0835b778
j1nma <j1nma@users.noreply.github.com>
2019-04-11 11:12:21 -0300

    done

--- scripts/trajectory-temperature.py
+++ scripts/trajectory-temperature.py
@@ -39,9 +39,30 @@ values = zeros(times);
 os.system('python3 ./scripts/generate_for_temperatures.py {N} {L} {velocity_modules} {small_radius} {small_mass} {big_radius} {big_mass}'.format(
 		N = N,
 		L = L,
-		velocity_modules = str(max_velocity_modules),
+		velocity_modules = str(max_velocity_modules).replace(']','').replace('[','').replace(' ',''),
 		small_radius = small_radius,
 		small_mass = small_mass,
 		big_radius = big_radius,
 		big_mass = big_mass
 		));
+
+for k in range(0, times):
+	command = 'java -jar ./target/molecular-dynamics-simulation-1.0-SNAPSHOT.jar --dynamicFile=./data/Dynamic-N={N}-V={max_velocity_module}.txt --staticFile=./data/Static-N={N}.txt --time={limitTime} --boxSize={L}'.format(
+						N = N,
+						max_velocity_module = max_velocity_modules[k],
+						limitTime = limitTime,
+						L = L,
+						)
+	print(command)
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0)
+	number = None;
+	p.stdout.readline(); # Results line
+	line = p.stdout.readlines() # Temperature line
+	number = line[0].decode()
+	number = number.split('\t')
+	number = number[1]
+	number = number.replace('\n', '')
+	values[k] = float(number)
+	print(values[k])
+	func = 'bigParticleTrajectory(' + str(k) + ',' + str(values[k]) + ')';
+	octave.eval(func)
